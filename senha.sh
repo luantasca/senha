134dc9967af585940f4ac2755126c502d93545a8
Luan Tasca <luan@sisbsd.com.br>
2018-06-24 21:40:42 -0300

    Versao nova

--- senha.sh
+++ senha.sh
@@ -4,7 +4,9 @@
 senha_suporte=`/usr/bin/pwgen -nB 7 1`
 senha_root=`/usr/bin/pwgen -nB 7 1`
 name=`/usr/bin/pwgen -nB 7 1`
-hostname=`hostname`
+hostname=`cat /etc/postfix/main.cf |grep "myhostname = " |awk {'print $3'}`
+porta_ssh=`cat /etc/ssh/sshd_config |grep "Port " |awk {'print $2'}`
+ip=`curl eth0.me`
 
 (echo $senha_suporte ; echo $senha_suporte ) | passwd suporte 
 (echo $senha_root ; echo $senha_root ) | passwd root
@@ -14,11 +16,14 @@ echo "" >> /tmp/senha.$name
 echo "Senha do Usuario Suporte: $senha_suporte" >> /tmp/senha.$name
 echo "Senha do Usuario Root: $senha_root" >> /tmp/senha.$name
 echo "" >> /tmp/senha.$name
+echo "IP do Servidor: $ip" >> /tmp/senha.$name
+echo "Porta SSH: $porta_ssh" >> /tmp/senha.$name
+echo "" >> /tmp/senha.$name
 echo "SisBSD Tecnologia" >> /tmp/senha.$name
 echo "www.sisbsd.com.br" >> /tmp/senha.$name
 
 # Manda email
-mail -s "Senha Servidor - $hostname" luan@sisbsd.com.br < /tmp/senha.$name
+mail -s "Senha Servidor - $hostname" senhas@sisbsd.com.br < /tmp/senha.$name
 rm -rf /tmp/senha.$name
 
 echo ""
